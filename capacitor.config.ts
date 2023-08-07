01a71eab8a4e7161993093361e63877b989b7f2b
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-08-07 20:26:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oauth2-test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
