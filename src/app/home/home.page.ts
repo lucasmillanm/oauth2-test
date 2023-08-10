c99b6103e0ff233245a5b692fc92f06683feff40
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-08-10 12:46:10 +0200

    add page init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserInfo} from '../models/user-info';
+import {GoogleApiService} from '../services/google-api.service';
+import {EmailIdResponse} from '../models/email-id-response';
+import {EmailResponse} from '../models/email-response';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  userInfo?: UserInfo;
+  emailIdResponse?: EmailIdResponse;
+  emailIds: string[] = [];
+  emailResponses: EmailResponse[] = [];
+  emailSnippets: string[] = [];
 
+  constructor(
+    private readonly googleApi: GoogleApiService
+  ) {
+    googleApi.userProfileSubject.subscribe((info: UserInfo) =>
+      this.userInfo = info
+    );
+  }
+
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn();
+  }
+
+  logOut(): void {
+    this.googleApi.signOut();
+  }
+
+  getEmails(): void {
+    if (this.userInfo?.info.sub) {
+      const sub: string = this.userInfo.info.sub;
+      this.googleApi.getEmailIds(sub).subscribe((ids) => {
+        this.emailIdResponse = ids;
+        this.transformResponseToId();
+        this.emailResponses = [];
+
+        console.log(this.emailResponses);
+
+      }, (error) => {
+        console.log(error);
+        return;
+      });
+    }
+  }
+  transformResponseToId(): void {
+    if (this.emailIdResponse) {
+      this.emailIds = this.emailIdResponse.messages.map(response => response.id);
+    } else {
+      return;
+    }
+  }
 }
